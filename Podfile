cbab58a1e0aaf29ea0627ae4a94f73e0daac94c0
Christians Bonilla <Christians.bonilla@fletx.co>
2022-09-11 10:19:06 -0500

    Pod Install SkyFloatingLabelTextField

--- Podfile
+++ Podfile
@@ -7,9 +7,10 @@ target 'CeibaContacts' do
 
   # Pods for CeibaContacts
 
-pod 'Alamofire', '~> 4.7'
+  pod 'Alamofire', '~> 4.7'
 	pod 'SQLite.swift', '~> 0.13.3'
  	pod 'lottie-ios'
+  pod 'SkyFloatingLabelTextField', '~> 3.0'
 
   target 'CeibaContactsTests' do
     inherit! :search_paths
--- Pods/Target Support Files/Pods-CeibaContacts/Pods-CeibaContacts-frameworks.sh
+++ Pods/Target Support Files/Pods-CeibaContacts/Pods-CeibaContacts-frameworks.sh
@@ -178,11 +178,13 @@ code_sign_if_enabled() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SQLite.swift/SQLite.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SkyFloatingLabelTextField/SkyFloatingLabelTextField.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/lottie-ios/Lottie.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SQLite.swift/SQLite.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SkyFloatingLabelTextField/SkyFloatingLabelTextField.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/lottie-ios/Lottie.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
